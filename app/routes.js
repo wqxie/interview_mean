93d1d20b99886bf413e1e400dec5ff689882a4f2
Weiqin Xie <wqxie@umich.edu>
2017-12-15 11:26:35 +0800

    change a little big

--- app/routes.js
+++ app/routes.js
@@ -3,11 +3,15 @@ module.exports = function(app) {
 	// server routes ===========================================================
 	// handle things like api calls
 	// authentication routes
+	app.get("/test", (req, res) => {
+		console.log("test");
+		res.send("ok");
+	});
 
 	// frontend routes =========================================================
 	// route to handle all angular requests
 	app.get('*', function(req, res) {
 		res.sendfile('./public/index.html');
 	});
-
+	
 };
--- public/js/appRoutes.js
+++ public/js/appRoutes.js
@@ -8,16 +8,6 @@ angular.module('appRoutes', []).config(['$routeProvider', '$locationProvider', f
 			controller: 'MainController'
 		})
 
-		.when('/nerds', {
-			templateUrl: 'views/nerd.html',
-			controller: 'NerdController'
-		})
-
-		.when('/geeks', {
-			templateUrl: 'views/geek.html',
-			controller: 'GeekController'	
-		});
-
 	$locationProvider.html5Mode(true);
 
 }]);
